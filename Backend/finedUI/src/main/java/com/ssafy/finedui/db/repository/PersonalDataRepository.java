437bbf9ef8362696092e53114f17d95d65156048
junhong625 <junhong625@naver.com>
2023-03-17 17:56:09 +0900

    [BE]:[refactor] PersonalData

--- Backend/finedUI/src/main/java/com/ssafy/finedui/db/repository/PersonalDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.finedui.db.repository;
-
-import com.ssafy.finedui.db.entity.PersonalData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonalDataRepository extends JpaRepository<PersonalData, Long> {
-}
--- Backend/finedUI/src/main/java/com/ssafy/finedui/db/repository/UploadFileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.finedui.db.repository;
-
-import com.ssafy.finedui.db.entity.UploadFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UploadFileRepository extends JpaRepository<UploadFile, Long> {
-}
