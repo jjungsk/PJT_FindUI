4cdf28426ed00f6ddcb7eb3af79ab4c20a54e247
석민지 <mjseok98@gmail.com>
2023-03-17 17:48:20 +0900

    BE:feat 채팅 데이터 저장 및 보여주기

---
+++ Chat/findedUIChat/src/main/java/com/ssafy/fineduichat/FinedUiChatApplication.java
@@ -0,0 +1,11 @@
+package com.ssafy.fineduichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinedUiChatApplication {
+
+    public static void main(String[] args) {SpringApplication.run(FinedUiChatApplication.class, args); }
+
+}
