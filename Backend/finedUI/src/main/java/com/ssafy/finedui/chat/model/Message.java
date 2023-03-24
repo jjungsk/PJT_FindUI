4b53a897f5b980ba5c4bc8d8114e81f417f00b6c
석민지 <mjseok98@gmail.com>
2023-03-24 14:43:53 +0900

    BE:feat 채팅 with STOMP,Kafka

---
+++ Backend/finedUI/src/main/java/com/ssafy/finedui/chat/model/Message.java
@@ -0,0 +1,46 @@
+package com.ssafy.finedui.chat.model;
+
+public class Message {
+    private String sender;
+    private String content;
+    private String timestamp;
+    public Message(){}
+
+    public Message(String sender, String content){
+        this.sender = sender;
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String Content() {
+        return content;
+    }
+
+    public void Content(String content) {
+        this.content = content;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "sender='" + sender + '\'' +
+                ", content='" + content + '\'' +
+                ", timestamp='" + timestamp + '\'' +
+                '}';
+    }
+}
