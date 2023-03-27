6dc246c9ac9081118e4a6c4fa389b418151a50f4
황진태 <jintea519@gmail.com>
2023-03-27 17:44:16 +0900

    [FE]:[FEAT] 등록 페이지

---
+++ Frontend/finedUI/src/components/screens/RegistScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+const modeDict = {
+  0: '사전 등록',
+  1: '실시간 실종자 등록',
+  2: '이산가족 등록',
+};
+
+const RegistScreen = ({mode = 0}) => {
+  return <SafeAreaView></SafeAreaView>;
+};
