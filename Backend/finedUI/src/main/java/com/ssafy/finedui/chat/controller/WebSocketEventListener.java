5bab9327538cc71d2dd3acd2accc0caf482b5942
석민지 <mjseok98@gmail.com>
2023-03-30 16:08:18 +0900

    BE:feat websocket활용한 채팅 백엔드

--- Backend/finedUI/src/main/java/com/ssafy/finedui/chat/controller/WebSocketEventListener.java
+++ Backend/finedUI/src/main/java/com/ssafy/finedui/chat/controller/WebSocketEventListener.java
@@ -1,6 +1,7 @@
 package com.ssafy.finedui.chat.controller;
 
-import com.ssafy.finedui.chat.model.Message;
+
+import com.ssafy.finedui.chat.model.ChatMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,30 +11,33 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
 @Component
 public class WebSocketEventListener {
+
     private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
 
     @Autowired
     private SimpMessageSendingOperations messagingTemplate;
 
     @EventListener
-    public void handleWebSocketConnectListener(SessionConnectedEvent event){
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
         logger.info("Received a new web socket connection");
     }
 
     @EventListener
-    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
         String username = (String) headerAccessor.getSessionAttributes().get("username");
-        if(username!=null){
-            logger.info("User Disconnected : "+username);
+        if(username != null) {
+            logger.info("User Disconnected : " + username);
 
-            Message message = new Message();
-            message.setType(Message.MessageType.LEAVE);
-            message.setSender(username);
+            ChatMessage chatMessage = new ChatMessage();
+            chatMessage.setType(ChatMessage.MessageType.LEAVE);
+            chatMessage.setSender(username);
 
-            messagingTemplate.convertAndSend("/topic/group",message);
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
         }
     }
 }
