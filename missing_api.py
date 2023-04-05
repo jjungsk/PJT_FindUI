986140052dc899067aff4128de6ac4a4f93f5464
junhong625 <junhong625@naver.com>
2023-04-05 17:56:58 +0900

    [BE]:[feat]실종자 더미 데이터 삽입(missing_api.py)

--- missing_api.py
+++ missing_api.py
@@ -1,9 +1,11 @@
-import requests, pymysql, pprint
+import requests, pymysql
+from PIL import Image 
+from io import BytesIO
 
 ## Kakao API 참고 주소(https://developers.kakao.com/docs/latest/ko/local/dev-guide)
 ## 안전 드림 API 참고 주소(https://www.safe182.go.kr/home/api/guide3.do)
 
-conn = pymysql.connect(host='127.0.0.1', user='ssafy', password='ssafy', db='finedui', charset='utf8')
+conn = pymysql.connect(host='j8a108.p.ssafy.io', user='ssafy', password='ssafy', db='finedui', charset='utf8')
 cur = conn.cursor()
 
 url = "https://www.safe182.go.kr/api/lcm/amberList.do"
@@ -17,24 +19,24 @@ res = requests.post(url, params=params).json()
 # pprint.pprint(res)
 # print(len(res["list"]))
 for missing in res["list"]:
-    print(f"https://www.safe182.go.kr/home/lcm/lcmMssGet.do?gnbMenuCd=014000000000&lnbMenuCd=014001000000&rptDscd=2&msspsnIdntfccd={missing['msspsnIdntfccd']}")
-    # add = missing['occrAdres'] # 실종 주소
+    image_url = f"https://www.safe182.go.kr/api/lcm/imgView.do?msspsnIdntfccd={missing['msspsnIdntfccd']}"
+    # image = Image.open(BytesIO(requests.get(image_url).content))
+    # image.save()
+    add = missing['occrAdres'] # 실종 주소
 
-    # kakao_url = "https://dapi.kakao.com/v2/local/search/address.json"
+    kakao_url = "https://dapi.kakao.com/v2/local/search/address.json"
 
-    # header = {"Authorization" : "KakaoAK b3f0f8e3b3eba6232aa894484881c143"}
+    header = {"Authorization" : "KakaoAK b3f0f8e3b3eba6232aa894484881c143"}
 
-    # params = {"query" : add}
+    params = {"query" : add}
 
-    # add_res = requests.get(url=kakao_url, headers=header, params=params).json()
+    add_res = requests.get(url=kakao_url, headers=header, params=params).json()
 
-    # print(add_res)
-    # if add_res['documents']:
-    #     add_res = add_res['documents'][0]
-    #     gender = 1 if missing['sexdstnDscd'] == "남자" else 2
-    #     missing_date = f"{missing['occrde'][:4]}-{missing['occrde'][4:6]}-{missing['occrde'][6:8]} 00:00:00.0000000"
-    #     # print(f"{missing['nm']} : {missing['']}")
-    #     print(missing)
-    #     cur.execute(f"INSERT INTO regist_info(gender, name, longitude, latitude, is_missing, missing_time) VALUES({gender}, '{missing['nm']}', {add_res['x']}, {add_res['y']}, 1, '{missing_date}')")
+    if add_res['documents']:
+        add_res = add_res['documents'][0]
+        gender = 1 if missing['sexdstnDscd'] == "남자" else 2
+        missing_date = f"{missing['occrde'][:4]}-{missing['occrde'][4:6]}-{missing['occrde'][6:8]} 00:00:00.0000000"
+        # print(f"{missing['nm']} : {missing['']}")
+        cur.execute(f"INSERT INTO regist_info(gender, name, longitude, latitude, is_missing, missing_time, front_image_path) VALUES({gender}, '{missing['nm']}', {add_res['x']}, {add_res['y']}, 1, '{missing_date}', '{image_url}')")
 
-# conn.commit()
+conn.commit()
